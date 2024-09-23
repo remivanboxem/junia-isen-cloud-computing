bd8651964acc688b83ab9a079b93585081db053e
Rémi <github@atremi.publictestregion.xyz>
2024-09-23 09:42:28 +0000

    finish

--- week-1/infra-as-code-discovery/terraform/main.tf
+++ week-1/infra-as-code-discovery/terraform/main.tf
@@ -2,11 +2,20 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> X.X.X"
+      version = "~> 4.3.0"
     }
   }
 }
 
 provider "azurerm" {
+  subscription_id = "idhere"
   features {}
+}
+
+resource "azurerm_resource_group" "CloudComputing" {
+  name     = "CloudComputing"
+  location = "France Central"
+  tags = {
+    "hello" = "world"
+  }
 }
